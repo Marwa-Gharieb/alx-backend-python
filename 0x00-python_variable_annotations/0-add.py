d3d1a298bb1e1b6d28292774c2b1066a2e078b1a
marwa <Ifonll>
2024-09-12 19:57:43 -0400

    :)

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 '''Basic annotations - add '''
 
-def add(a: float,b: float)-> float:
+
+def add(a: float, b: float) -> float:
     '''function add that takes float arguments and return the sum of them '''
     c = a + b
     return c
