ae746ad7553cb4e1aebdf7b989094a7810fe2bbd
marwa <Ifonll>
2024-09-13 19:46:33 -0400

    :)

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-'''Duck typing - first element of a sequence'''
+'''Task 10's module.
+'''
+from typing import Any, Sequence, Union
 
 
-from typing import Sequence, Any, Union, Tuple
-from types import NoneType
-
-
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
-    '''The types of the elements of the input are not know'''
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Retrieves the first element of a sequence if it exists.
+    '''
     if lst:
         return lst[0]
     else:
