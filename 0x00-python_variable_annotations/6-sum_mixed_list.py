55bf9e08664bb75d5342cedc14ac2c6c06d6f797
marwa <Ifonll>
2024-09-12 21:42:45 -0400

    :)

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''Complex types - mixed list '''
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     '''return sum of list of floats and integers'''
     result = 0
     for arg in mxd_lst:
