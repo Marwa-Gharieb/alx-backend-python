ebab2412de24d0e7c133fba9ec8cf2c8915c49b6
marwa <Ifonll>
2024-09-12 20:32:59 -0400

    :)

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Basic annotations - floor '''
+import math
+
+
+def floor(a: float) -> int:
+    '''a func takes a float n as argument and returns the float floor'''
+    return math.floor(a)
