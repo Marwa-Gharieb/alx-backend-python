26914eb80f021a695864e4fba1419d0d29d32217
marwa <Ifonll>
2024-09-12 21:08:03 -0400

    :)

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,17 +2,7 @@
 '''Define variables '''
 
 
-a = 1
-# an integer with a value of 1
-
-
-pi = 3.14
-# a float with a value of 3.14
-
-
-i_understand_annotations = True
-# a boolean with a value of True
-
-
-school = 'Holberton'
-# a string with a value of “Holberton”
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
