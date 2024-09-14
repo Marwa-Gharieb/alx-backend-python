80b8ee796956e69bceaba7d86ea40cc58727cdd4
marwa <Ifonll>
2024-09-14 19:52:45 -0400

    :)

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 '''Type Checking '''
-
-from typing import List, Tuple, Iterable, Sequence
+from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
