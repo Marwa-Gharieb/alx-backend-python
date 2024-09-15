665f1b4ffb533099e1dabf89c10ee7c26eaed7ea
marwa <Ifonll>
2024-09-14 21:15:29 -0400

    :)

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Let's execute multiple coroutines at the same time with async'''
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    ''' a function that runs another function a specific times'''
+    list = []
+    list = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return list
